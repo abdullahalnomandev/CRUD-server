4189d5c5dc653daadcdc8ca1d44aac54d24fbb1b
Abdullah Al Noman <abdullahalnoman1512@gmail.com>
2021-06-17 23:40:33 +0600

    project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const objectId = require('mongodb').ObjectID
-const port = 5000 || process.env.PORT ;
+const port = 3050 || process.env.PORT ;
 
 
 require('dotenv').config()
